bedece7f916e3096f63d29b7253896c2f112a442
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-12-11 12:00:54 +0200

    New ProductionCenterModel

---
+++ Inventar.Data.Models/ProductionCenter.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+public class ProductionCenter
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = "Central production unit";
+
+    public CenterStatus Status { get; set; } = CenterStatus.Operational;
+
+    public string Location { get; set; }
+
+    public string Capacity { get; set; }
+
+    public string Contact { get; set; }
+
+    public string Expenses { get; set; }
+}
+
+public enum CenterStatus
+{
+    Operational,
+    Maintenance,
+    UnderConstruction,
+    Closed
+}
