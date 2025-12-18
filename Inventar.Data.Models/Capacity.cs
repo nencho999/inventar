3f88b8638308465ccad8456db6d84ad42a159807
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-12-18 12:34:31 +0200

    no change

--- Inventar.Data.Models/Capacity.cs
+++ Inventar.Data.Models/Capacity.cs
@@ -6,7 +6,6 @@
 
         public Guid PrimaryMaterialBaseId { get; set; }
         public PrimaryMaterialBase PrimaryMaterialBase { get; set; } = null!;
-
         public Guid MaterialId { get; set; }
         public Material Material { get; set; } = null!;
         public decimal CapacityLimit { get; set; }
