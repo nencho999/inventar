d1cd2ffa128777965a383ab64b265deb31298484
Yordan Pavlov <iordan_pavlov1@abv.bg>
2025-12-18 12:20:48 +0200

    DropDownViewModel for materials

---
+++ Inventar.Web.ViewModels/MaterialDropDownModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventar.Web.ViewModels
+{
+    public class MaterialDropDownModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
